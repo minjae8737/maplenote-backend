b61304fdc39fcb817dd8e3d08df5b8c769a20129
Aljae <minjae8737@gmail.com>
2025-03-24 13:45:04 +0900

    Initial commit

---
+++ src/main/java/com/project/maplenote/MaplenoteApplication.java
@@ -0,0 +1,13 @@
+package com.project.maplenote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaplenoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaplenoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/maplenote/MaplenoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.maplenote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaplenoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
